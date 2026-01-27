be7098c81d55950a210c3fe0d3c95ad3182c6d02
Gurjyot Singh <gurjyot.singh2025@students.sau.ac.in>
2026-01-27 14:44:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4086204628-7e41a",
+  "appId": "1:301124877262:web:b5d1414eea7e4edbd9499f",
+  "apiKey": "AIzaSyCeHvhrNWfMpJklcPPtzuEh4cH6AFr2EvU",
+  "authDomain": "studio-4086204628-7e41a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "301124877262"
+};
